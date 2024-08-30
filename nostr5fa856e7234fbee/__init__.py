f556b397fae663b9103d9c6b73a625dfcac84de6
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-08-30 17:48:54 +0200

    Update __init__.py

--- nostr5fa856e7234fbee/__init__.py
+++ nostr5fa856e7234fbee/__init__.py
@@ -24,11 +24,10 @@ import bech32
 
 RANDOM_NUMBER_OF_RELAYS_TO_USE = 1
 DEFAULT_OLDNESS_SECONDS = 1000
-DEFAULT_MAXIMUM_ITEMS = 30
+DEFAULT_MAXIMUM_ITEMS = 10
 DEFAULT_MIN_POST_LENGTH = 10
 ALL_RELAYS = {
     "wss://relay.nostrdice.com",
-    "wss://lunchbox.sandwich.farm",
     "wss://relay.snort.social",
     "wss://relay.current.fyi",
     "wss://nostr.x0f.org",
@@ -112,11 +111,10 @@ def encode_nostr_event(event_id, relays=None, pubkey=None):
     return bech32.bech32_encode(hrp, bech32.convertbits(data, 8, 5))
 
 
-async def parse_nostr():
+async def parse_nostr(min_post_length, max_items=DEFAULT_MAXIMUM_ITEMS):
     relay_manager = RelayManager(timeout=2)
     try:
         logging.basicConfig(level=logging.INFO)
-        nest_asyncio.apply()  # allows for nested async loops, essential here
         select_relayed = random.sample(list(ALL_RELAYS), RANDOM_NUMBER_OF_RELAYS_TO_USE)
         for relay in select_relayed:
             logging.info("Adding relay" + relay)
@@ -130,31 +128,44 @@ async def parse_nostr():
         relay_manager.add_subscription_on_all_relays(subscription_id, filters)
         relay_manager.run_sync()
 
+        processed_items = 0
         current_ids = []
         content_checker = []  # some of the content is posted multiple times on different relays at the same time, with different ids
         while relay_manager.message_pool.has_events():
+            logging.info(f"[NOSTR] Processing {processed_items} / {max_items} items")
+            if processed_items >= max_items:
+                break
+
             event_msg = relay_manager.message_pool.get_event()
             date = convert_to_datetime_from_timestamp(event_msg.event.created_at)
             # remove 2 hours from the date to adjust for the time difference between the server and the local time
             date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.00Z") - timedelta(hours=2)
-            # back to  string
             date = date.strftime("%Y-%m-%dT%H:%M:%S.00Z")
+
+            content_ = event_msg.event.content
 
 
             if check_for_max_age_with_correct_format(date, DEFAULT_OLDNESS_SECONDS):
-                if event_msg.event.id not in current_ids and event_msg.event.content not in content_checker:  # new item that we can select
+                if event_msg.event.id not in current_ids and content_ not in content_checker:  # new item that we can select
                     current_ids.append(event_msg.event.id)
                     # convert the ID to bech32
                     bech32_id = encode_nostr_event(event_id=str(event_msg.event.id), relays=select_relayed)
-                    logging.info(f"[NOSTR] POST ID: {bech32_id} - Content: {event_msg.event.content}")
-                    content_checker.append(event_msg.event.content)
+                    content_checker.append(content_)
+                                
+                    if len(content_) < min_post_length:
+                        continue
                     yield Item(
-                        content=Content(str(event_msg.event.content)),
+                        content=Content(str(content_)),
                         created_at=CreatedAt(date),
                         url=Url("https://nostr.social/"  + str(bech32_id)),
                         domain=Domain("nostr.social"),
                         external_id=ExternalId(str(bech32_id))
                     )
+                    processed_items += 1
+                    logging.info(f"[NOSTR] POST ID: {bech32_id} - {date} - Content: {content_} {processed_items} / {max_items} items") # log the content of the post
+                    if processed_items >= max_items:
+                        logging.info(f"[NOSTR] Finished scraping {max_items} items")
+                        break
     except Exception as e:
         logging.exception(f"Error: {str(e)}")
     finally:
@@ -162,13 +173,13 @@ async def parse_nostr():
 
 
 async def query(parameters: dict) -> AsyncGenerator[Item, None]:
+    nest_asyncio.apply()  # allows for nested async loops, essential here
     yielded_items = 0
     max_oldness_seconds, maximum_items_to_collect, min_post_length = read_parameters(parameters)
     logging.info(f"[nostr.social] - Scraping ideas posted less than {max_oldness_seconds} seconds ago.")
 
-    async for item in parse_nostr():
+    async for item in parse_nostr(min_post_length, maximum_items_to_collect):
         yielded_items += 1
         yield item
-        logging.info(f"[nostr.social] Found new post :\t posted at {item.created_at}, URL = {item.url}")
         if yielded_items >= maximum_items_to_collect:
             break
