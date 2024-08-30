dad4e89ce110a07c0473787b5e3beb0b6dadd033
Mathias Dail <93382891+MathiasExorde@users.noreply.github.com>
2024-08-30 18:11:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import find_packages, setup
 
 setup(
     name="nostr5fa856e7234fbee",
-    version="0.0.7",
+    version="0.0.8",
     packages=find_packages(),
     install_requires=[
         "exorde_data",
         "aiohttp",
         "beautifulsoup4>=4.11",
         "pynostr>=0.6.2",
-        "nest_asyncio>=1.5.6"
+        "nest_asyncio==1.5.6",
+        "bech32==1.2.0"
     ],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
 )
